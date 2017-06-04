f91f117f8796a54941e78466451a46295a900b48
CarolPC <carolina.pascale.c@gmail.com>
2017-06-03 22:39:40 -0300

    The user is the current user

--- cloneinstagram/app/controllers/posts_controller.rb
+++ cloneinstagram/app/controllers/posts_controller.rb
@@ -26,6 +26,8 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
+    @post.user = current_user
+
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
