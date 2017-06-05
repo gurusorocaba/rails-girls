4b698fd81057a498ba849b0acd48af5a7104c0da
CarolPC <carolina.pascale.c@gmail.com>
2017-06-04 22:25:25 -0300

    Add pagination to controller, 2 posts per page

--- cloneinstagram/app/controllers/posts_controller.rb
+++ cloneinstagram/app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.paginate(:page => params[:page], :per_page => 2)
   end
 
   # GET /posts/1
